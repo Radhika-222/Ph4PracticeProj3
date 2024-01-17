565fa596ccced681d10e6ab0eb4ad0966b46c951
Radhika-222 <wandareradhika@gmail.com>
2024-01-17 19:47:02 +0530

    Ph4PracticeProj3

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ph4PracticeProj3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
